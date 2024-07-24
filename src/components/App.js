141383d142dcc3af7d3de438e3362a2222c1666e
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-07-24 10:55:55 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,30 @@
+import React,{useState} from 'react';
 
+const App = () => {
+  const [activeTab, setActiveTab] = useState('Home');
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
+  return (
+    <div>
+      <div className="tabs">
+        <div id="Home-tab" className="tab" onClick={() => handleTabClick('Home')}>
+          Home
+        </div>
+        <div id="About-tab" className="tab" onClick={() => handleTabClick('About')}>
+          About
+        </div>
+        <div id="Features-tab" className="tab" onClick={() => handleTabClick('Features')}>
+          Features
+        </div>
+      </div>
+      <div className="viewport">
+        {activeTab} Page Goes Here
+      </div>
+    </div>
+  );
+};
+
+export default App;
